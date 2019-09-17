ecfc7a67af6f927b3f0193d9d60bd103f55fb39c
Skyler O'Neill <one2@ad.umbc.edu>
2019-09-17 13:01:14 -0400

    Made vending machine healing

---
+++ Assets/Scripts/Components/HealOnTouch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealOnTouch : MonoBehaviour
+{
+    [SerializeField]
+    private int amount = 25;
+    [SerializeField]
+    private string healTag = "Player";
+    [SerializeField]
+    private bool destroyAfterHealing = false;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // Heal the gameobject we collided with if it is tagged with the heal tag and it has health
+        if(other.gameObject.tag == healTag && other.gameObject.GetComponent<Health>())
+        {
+            other.gameObject.GetComponent<Health>().Heal(amount);
+        }
+    }
+
+    private void OnTriggerEnter(Collision other)
+    {
+        // Heal the gameobject we collided with if it is tagged with the heal tag and it has health
+        if (other.gameObject.tag == healTag && other.gameObject.GetComponent<Health>())
+        {
+            other.gameObject.GetComponent<Health>().Heal(amount);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Health))]
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    private Text deathText;
+    [SerializeField]
+    private string deathMessage = "You would be dead.";
+
+    private Health hp;
+
+    private void Start()
+    {
+        hp = GetComponent<Health>();
+        if (deathText == null) deathText = GameObject.FindGameObjectWithTag("DeathText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        // Temporary hack
+        if(hp.GetHealth() <= 0)
+        {
+            // Lock health at 1
+            hp.SetHealth(1);
+
+            // Display the death message
+            if (deathText != null)
+                deathText.text = deathMessage;
+        }
+        else if(hp.GetHealth() > 1 && deathText != null)
+        {
+            // Hide the death message
+            deathText.text = "";
+        }
+    }
+}
