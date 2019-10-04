5922257fd366f5e24e473c23b8b17accf10d8611
Skyler O'Neill <one2@ad.umbc.edu>
2019-10-04 16:18:54 -0400

    Added health bubble controller to player, health bubble now scales with player health

--- Assets/Scripts/HUD/HUDHealthBubble.cs
+++ Assets/Scripts/HUD/HUDHealthBubble.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HUDImageHealth : MonoBehaviour
+public class HUDHealthBubble : MonoBehaviour
 {
+    public bool debug = false;
     [Tooltip("The image that will shrink based upon health levels.")]
     [SerializeField]
     private Image heartImage;
+    [SerializeField]
+    private Text valueText;
     [Tooltip("The rate at which the heart image scale will move from the new to the old.")]
     [Range(0f, 1f)]
     [SerializeField]
-    private float interpolant = 0.75f;
+    private float interpolant = 0.1f;
     [Tooltip("Once within this distance to the target scale the heart image will snap to the target.")]
     [SerializeField]
     private float snapThreshold = 0.05f;
@@ -27,10 +30,24 @@ public class HUDImageHealth : MonoBehaviour
     {
         baseScale = heartImage.rectTransform.localScale;
         targetScale = Vector3.one;
+
+        // Ensure there is a heart image
+        if (heartImage == null)
+            heartImage = gameObject.GetComponentInChildren<Image>();
+
+        // Ensure there is value text
+        if (valueText == null)
+            valueText = gameObject.GetComponentInChildren<Text>();
     }
 
     private void Update()
     {
+        // Don't do anything if we don't have a heart image or value text
+        if (heartImage == null || valueText == null)
+            return;
+
+        valueText.text = "" + currentValue;
+
         // Calculate the new scale
         targetScale = baseScale * ((float)currentValue / (float)maxValue);
 
@@ -40,5 +57,31 @@ public class HUDImageHealth : MonoBehaviour
         // Snap to the target scale if within the threshold
         if (Mathf.Abs(heartImage.rectTransform.localScale.magnitude - targetScale.magnitude) < snapThreshold)
             heartImage.rectTransform.localScale = targetScale;
+    }
+
+    public bool SetMaxvalue(int newMax)
+    {
+        // The new max value must be larger than zero
+        if (newMax < 1)
+        {
+            if (debug) Debug.Log(newMax + " is not a valid new max value. It must be > 0.");
+            return false;
+        }
+
+        maxValue = newMax;
+        return true;
+    }
+
+    public bool SetCurrentvalue(int newVal)
+    {
+        // The new value must be larger than zero
+        if (newVal < 1)
+        {
+            if (debug) Debug.Log(newVal + " is not a valid new value. It must be > 0.");
+            return false;
+        }
+
+        currentValue = newVal;
+        return true;
     }
 }
---
+++ Assets/Scripts/Player/HealthHUDController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class HealthHUDController : MonoBehaviour
+{
+    public bool debug = false;
+    [Tooltip("The tag of the health bubble that this script will modify. Only used if healthBubble is not set.")]
+    [SerializeField]
+    private string healthBubbleTag = "HealthBubble";
+    [Tooltip("The health bubble that this script will modify. If not set then will be set to the above tag.")]
+    [SerializeField]
+    private HUDHealthBubble healthBubble;
+
+    private Health hp;
+
+    private void Start()
+    {
+        hp = gameObject.GetComponent<Health>();
+
+        // Set the health bubble via tag if no other health bubble was given
+        if (healthBubble == null)
+            healthBubble = GameObject.FindGameObjectWithTag(healthBubbleTag).GetComponent<HUDHealthBubble>();
+
+        if (healthBubble == null && debug)
+            Debug.Log("Setting health bubble according to tag failed."); 
+    }
+
+    private void Update()
+    {
+        // Set health bubble value
+        healthBubble.SetCurrentvalue(hp.GetHealth());
+    }
+}
